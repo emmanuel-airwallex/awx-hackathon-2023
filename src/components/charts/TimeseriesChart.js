735d91230eb73f26301c24736b2d977e49eedf73
percy-awx <percy.ding@airwallex.com>
2023-06-22 17:30:48 +1000

    add conversion data points

--- src/components/charts/TimeseriesChart.js
+++ src/components/charts/TimeseriesChart.js
@@ -1,4 +1,39 @@
 import { ResponsiveLine } from '@nivo/line'
+import { Bar } from '@nivo/bar'
+import React from "react";
+const barColor = "#bd7a0f";
+
+const data = [
+    { x: "0", v: 2.7 },
+    { x: "1", v: 3.3 },
+    { x: "2", v: 3.8 },
+    { x: "3", v: 4.3 },
+    { x: "4", v: 1.7 },
+    { x: "5", v: 2.2 },
+    { x: "6", v: 5.5 },
+    { x: "7", v: 6.0 }
+];
+
+const Scatter = ({ bars, xScale, yScale }) => {
+    console.log("printing");
+    console.log(bars[0].data.data.x);
+    console.log(bars[0].data.data.v);
+    return (
+        <>
+            {bars.map((bar) => (
+                <circle
+                    key={`point-${bar.data.data.x}`}
+                    cx={xScale(bar.data.index) + bar.width / 2}
+                    cy={yScale(bar.data.data.v + 0.2)}
+                    r={3}
+                    fill="black"
+                    stroke="black"
+                    style={{ pointerEvents: "none" }}
+                />
+            ))}
+        </>
+    );
+};
 
 export const TimeseriesChart = ({ data }) => {
   return (
@@ -65,7 +100,31 @@ export const TimeseriesChart = ({ data }) => {
                 ]
             }
         ]}
+
+        // layers={["grid", "axes", "lines", "points", "markers", "legends"]}
       />
+        {/*<Bar*/}
+        {/*    width={500}*/}
+        {/*    height={400}*/}
+        {/*    data={data}*/}
+        {/*    keys={["y"]}*/}
+        {/*    maxValue={6.6}*/}
+        {/*    padding={0.6}*/}
+        {/*    margin={{*/}
+        {/*        top: 10,*/}
+        {/*        right: 10,*/}
+        {/*        bottom: 36,*/}
+        {/*        left: 36*/}
+        {/*    }}*/}
+        {/*    indexBy="x"*/}
+        {/*    enableLabel={false}*/}
+        {/*    colors={[barColor]}*/}
+        {/*    borderRadius={2}*/}
+        {/*    axisLeft={{*/}
+        {/*        tickValues: 7*/}
+        {/*    }}*/}
+        {/*    layers={["grid", "axes", "bars", Scatter]}*/}
+        {/*/>*/}
     </div>
   )
 }
--- src/pages/Conversions.js
+++ src/pages/Conversions.js
@@ -1,8 +1,21 @@
 import React from "react"
-import { TimeseriesChart } from '../components/charts/TimeseriesChart'
+import {TimeseriesChart} from '../components/charts/TimeseriesChart'
 import tradeData from "../data/tradeData"
 
 export const Conversions = () => {
+
+    for (var i = 0; i < tradeData.length; i++) {
+        for(var j = 0; j < tradeData[i].data.length; j++) {
+            var date = new Date(tradeData[i].data[j].x); // create Date object
+            var options = {
+                year: 'numeric', month: 'numeric', day: 'numeric',
+            };
+            tradeData[i].data[j].x = date.toLocaleDateString('en', options);
+        }
+    }
+
+    console.log(tradeData);
+
     return (
         <div>
             <h1>
