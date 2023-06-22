960203eac0b37fa593ea83c8b766697c3c04c6c4
DanielBlaker <daniel.blaker@airwallex.com>
2023-06-22 22:32:51 +1000

    Bot typing and waiting status indicators

--- src/chat/Container.js
+++ src/chat/Container.js
@@ -5,6 +5,7 @@ import Message from "./Message"
 import { conversionPrompt, expensesPrompt } from "../data/prompts";
 import ChatToggleButton from "./ChatToggleButton";
 import VisualisationPanel from "./VisualisationPanel";
+import WaitingStatus from "./WaitingStatus"
 
 function Container() {
     const location = useLocation();
@@ -14,14 +15,29 @@ function Container() {
     const [visualisation, setVisualisation] = useState({
         title: "Visualisation of rates"
     })
+    const [isBotTyping, setBotTyping] = useState(false)
+
     const [messages, setMessage] = useState([location.pathname === "/conversions" ? conversionPrompt[0] : expensesPrompt[0]]);
     const isContextConversions = location.pathname === "/conversions"
 
     const send = () => {
         if (inputValue.trim() == "") return;
-        setIsVisualisationOpen(!isVisualisationOpen)
         setInput("");
-        setMessage(messages => [...messages, { 'user': 'user', prompt: inputValue }, { 'user': 'operator', prompt: isContextConversions ? conversionPrompt[messages.length + 1].prompt : expensesPrompt[messages.length + 1].prompt }])
+        setMessage(messages => [...messages, { 'user': 'user', prompt: inputValue }])
+
+        setTimeout(() => {
+            // Time to start responding
+            setBotTyping(true)
+
+            setTimeout(() => {
+                // Time to respond
+                setMessage(messages => [...messages, { 'user': 'operator', prompt: isContextConversions ? conversionPrompt[messages.length].prompt : expensesPrompt[messages.length].prompt }])
+                setIsVisualisationOpen(!isVisualisationOpen)
+                setBotTyping(false)
+
+            }, 2000 + 2*Math.random())
+
+        }, 1000 + Math.random())
     }
 
     const onChangeHandler = event => {
@@ -48,20 +64,27 @@ function Container() {
             <ChatToggleButton onClick={() => setIsChatOpen(!isChatOpen)} isOpen={isChatOpen} />
             <VisualisationPanel isOpen={isVisualisationOpen && isChatOpen} visualisation={visualisation} />
             {
-                <div className={"fixed bottom-24 right-0 m-4 p-8 h-3/4 max-w-lg w-full m-8 transition-all " + (isChatOpen ? "-translate-x-3 opacity-100" : "opacity-0")} >
-                    <div className="bg-gray-50 m-4 p-8 ring-2 rounded ring-gray-100 w-full h-full min-h-full flex flex-col shadow-lg">
+                <div className={"fixed bottom-24 right-0 m-4 p-8 h-3/4 max-w-lg w-full m-8 transition-all duration-200 " + (isChatOpen ? "-translate-x-3 opacity-100" : "opacity-0")} >
+                    <div className="bg-gray-50 m-4 p-8 ring-2 rounded ring-gray-100 w-full h-full min-h-full flex flex-col shadow-2xl">
                         <h1 className="font-bold text-gray-700 mb-4">Airpilot</h1>
                         <hr className="mb-4" />
                         <div className="block w-full overflow-y-scroll no-scrollbar p-4">
                             <div className="flex flex-col">
-                                {messages.map((message, i) => { return <Message key={i} fromBot={message.user === "operator"} message={message.prompt} /> })}
+                                {messages.map((message, i) => {
+                                    return <Message key={i} visible={true} fromBot={message.user === "operator"}>
+                                        {message.prompt}
+                                    </Message>
+                                })}
+                                <Message key={"Bot Typing"} visible={isBotTyping} fromBot={true}>
+                                    <WaitingStatus />
+                                </Message>
                             </div>
                             <span className="mb-8" ref={messagesEndRef}></span>
                         </div>
                         <div className="block mt-auto bottom-10 pt-4 w-full">
                             <div className="flex flex-row h-10">
                                 <input className="flex-grow text-lg w-full p-4 mr-4 ring-2 rounded ring-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-red-200" placeholder="Ask a question..." onKeyDown={onChangeHandler} onChange={onChangeHandler} value={inputValue} />
-                                <svg xmlns="http://www.w3.org/2000/svg" classNames="w-8 h-8 absolute right-14 bottom-4" style={{"cursor":"pointer"}} height="100%" onClick={send} viewBox="0 0 512 512">
+                                <svg xmlns="http://www.w3.org/2000/svg" classNames="w-8 h-8 absolute right-14 bottom-4" style={{ "cursor": "pointer" }} height="100%" onClick={send} viewBox="0 0 512 512">
                                     <path d="M470.3 271.15L43.16 447.31a7.83 7.83 0 01-11.16-7V327a8 8 0 016.51-7.86l247.62-47c17.36-3.29 17.36-28.15 0-31.44l-247.63-47a8 8 0 01-6.5-7.85V72.59c0-5.74 5.88-10.26 11.16-8L470.3 241.76a16 16 0 010 29.39z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
                                 </svg>
                             </div>
--- src/chat/VisualisationPanel.js
+++ src/chat/VisualisationPanel.js
@@ -7,8 +7,8 @@ import expensesBreakdownData from '../data/expensesBreakdownByEmployeeData'
 function VisualisationPanel(params) {
     const isOpen = params.isOpen
     return <>
-        <div className={"fixed bottom-36 right-0 m-4 p-8 h-2/3 max-w-5xl w-full m-8 transition-all " + (isOpen ? "-translate-x-[450px] opacity-100" : "opacity-0")} >
-            <div className="bg-gray-50 m-4 p-8 ring-2 rounded ring-gray-100 w-full h-full min-h-full flex flex-col shadow-lg">
+        <div className={"fixed bottom-36 right-0 m-4 p-8 h-2/3 max-w-5xl w-full m-8 transition-all duration-300 " + (isOpen ? "-translate-x-[450px] opacity-100" : "-translate-x-[300px] opacity-0")} >
+            <div className="bg-gray-50 m-4 p-8 ring-2 rounded ring-gray-100 w-full h-full min-h-full flex flex-col shadow-xl">
                 <h1 className="text-2xl font-bold mb-4 text-gray-700">{params.visualisation.title}</h1>
                 <hr />
                 <TimeseriesChart data={tradeData} height={"100%"}/>
